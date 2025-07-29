ff40b84540806bc0d5276d3a7005b5fc4f84a342
BolasUlamog <113706845+BolasUlamog@users.noreply.github.com>
2025-07-28 21:33:42 -0700

    asdflk

---
+++ 6-months/game.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[50];
+    char password[] = "passw0rD123";
+
+    printf("Enter the password: ");
+    fgets(input, 50, stdin);
+    input[strcspn(input, "\n")] = '\0';
+
+    if (strcmp(input, password) == 0) {
+        printf("Welcome! The flag is: CTF{simple_password}\n");
+    } else {
+        printf("Wrong password! Try again.\n");
+    }
+
+    return 0;
+}
