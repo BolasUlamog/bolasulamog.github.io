33f696e15ecedd8243f6e2561413bc1c632b314b
BolasUlamog <113706845+BolasUlamog@users.noreply.github.com>
2025-07-28 18:30:41 -0700

    done

--- 6-months/ctf7key.py
+++ 6-months/ctf7key.py
@@ -5,7 +5,7 @@ import time
 target_hash = input("Paste hash: ").strip()
 current_time = int(time.time())
 
-for delta in range(-300, 300):  # Check ±5 minutes
+for delta in range(-300, 300): # 5 minutes before and after
     random.seed(current_time + delta)
     flag = f"flag{{{'%064x' % random.getrandbits(256)}}}"
     if hashlib.sha256(flag.encode()).hexdigest() == target_hash:
